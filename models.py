ffddebf57b4b97b0cf385df0db054ffc0968280b
Archimedes Li <escapereovery@gmail.com>
2024-08-24 12:05:27 -0400

    Pytorch implementation

---
+++ models.py
@@ -0,0 +1,48 @@
+import torch
+import numpy as np
+
+from torch import nn
+
+class MusicLSTM(nn.Module):
+    def __init__(self, input_len, hidden_size, num_classes, num_layers):
+        super(MusicLSTM, self).__init__()
+        self.hidden_size = hidden_size
+        self.num_layers = num_layers
+        self.num_classes = num_classes
+        self.lstm = nn.LSTM(input_len, hidden_size, num_layers, dropout= 0.2, batch_first=True)
+        self.lin = nn.Linear(hidden_size, num_classes)
+    
+    def forward(self, X):
+        hidden_states = torch.zeros(self.num_layers, X.size(0), self.hidden_size)
+        cell_states = torch.zeros(self.num_layers, X.size(0), self.hidden_size)
+        out, _ = self.lstm(X, (hidden_states, cell_states))
+
+        out = out[:, -1, :] #extract final hidden state; selects [all batches, last time step, all features]
+        out = self.lin(out)
+        return out
+    
+
+
+class GeminiLSTM(nn.Module):
+    def __init__(self, input_size, hidden_size, num_layers, num_classes):
+        super(GeminiLSTM, self).__init__()
+        self.lstm1 = nn.LSTM(input_size, hidden_size, num_layers, dropout=0.3, batch_first=True)
+        self.lstm2 = nn.LSTM(hidden_size, hidden_size, num_layers, dropout=0.3, batch_first=True)
+        self.lstm3 = nn.LSTM(hidden_size, hidden_size, num_layers, batch_first=True)
+        self.fc1 = nn.Linear(hidden_size, 256)
+        self.bn1 = nn.BatchNorm1d(256)
+        self.dropout1 = nn.Dropout(0.3)
+        self.fc2 = nn.Linear(256, num_classes)
+        #self.softmax = nn.Softmax(dim=1)
+
+    def forward(self, x):
+        x, _ = self.lstm1(x)
+        x, _ = self.lstm2(x)
+        x, _ = self.lstm3(x)
+        x = x[:, -1, :]
+        x = self.fc1(x)
+        x = self.bn1(x)
+        x = self.dropout1(x)
+        x = self.fc2(x)
+        #x = self.softmax(x)
+        return x
